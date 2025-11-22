1aee0df8fb1d08b13798500f86bfc6da2c060494
gump <contact@gump.dev>
2025-11-22 15:16:55 -0300

    update docs

--- library/sucata/camera.lua
+++ library/sucata/camera.lua
@@ -1,30 +1,28 @@
 ---@class Camera
-local camera = {}
+sucata.camera = {}
 
 ---Gets the current camera position
 ---@return number x # X coordinate of the camera position
 ---@return number y # Y coordinate of the camera position
-function camera.get_camera_position() end
+function sucata.camera.get_camera_position() end
 
 ---Sets the camera position
 ---@param x number # X coordinate of the camera position
 ---@param y number # Y coordinate of the camera position
-function camera.set_camera_position(x, y) end
+function sucata.camera.set_camera_position(x, y) end
 
 ---Gets the current camera rotation
 ---@return number rotation # Camera rotation in radians
-function camera.get_camera_rotation() end
+function sucata.camera.get_camera_rotation() end
 
 ---Sets the camera rotation
 ---@param rotation number # Camera rotation in radians
-function camera.set_camera_rotation(rotation) end
+function sucata.camera.set_camera_rotation(rotation) end
 
 ---Gets the current camera zoom
 ---@return number zoom # Camera zoom level
-function camera.get_camera_zoom() end
+function sucata.camera.get_camera_zoom() end
 
 ---Sets the camera zoom
 ---@param zoom number # Camera zoom level
-function camera.set_camera_zoom(zoom) end
-
-return camera
+function sucata.camera.set_camera_zoom(zoom) end
