0e7b2e7cf955c319eadcdd61a95e4dcd078f3ce3
gump <contact@gump.dev>
2026-02-03 21:20:13 -0300

    add load_shader

--- library/sucata/graphic.lua
+++ library/sucata/graphic.lua
@@ -15,3 +15,9 @@ function sucata.graphic.draw_text(props) end
 ---Set the background color of the window
 ---@param color string Hex color string (e.g., "#RRGGBB" or "#RRGGBBAA")
 function sucata.graphic.set_background_color(color) end
+
+---Loads a sucata shader
+---@param path string File path to the sucata shader
+---@param name string|nil Optional shader name
+---@return string shader_name The name of the shader
+function sucata.graphic.load_shader(path, name) end
