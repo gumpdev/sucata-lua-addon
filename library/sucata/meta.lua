619c02a76d80d4b32a135b9d2b070d0d5f7651cf
gump <contact@gump.dev>
2026-04-12 12:51:39 -0300

    add meta

---
+++ library/sucata/meta.lua
@@ -0,0 +1,13 @@
+---@meta
+
+---The meta module of sucata game engine
+---@class sucata.meta
+sucata.meta = {}
+
+--- The current OS 
+-- @field OS string
+sucata.meta.OS = ...
+
+--- The current version of Sucata Engine.
+-- @field VERSION string
+sucata.meta.VERSION = ...
